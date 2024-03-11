4f59c241f0732be0bf54c5a160c625756e2de077
Dillon Dains <dilloncsbb@gmail.com>
2024-03-11 10:18:57 -0500

    initial commit

---
+++ whiteboard/whiteboard.py
@@ -0,0 +1,7 @@
+# Create a function that given a list which represents street lights given as a parameter(l_street), determine if an outage has occurred. A street with a total number of "F" greater than or equal to 2 returns "Outage", anything below returns "Power"
+# Example Input: [ 'T', 'F', 'F', 'F' ]				
+# Example Output: "Outage"
+
+
+def solution(arr):
+    return 'Outage' if arr.count('F') >= 2 else 'Power'
